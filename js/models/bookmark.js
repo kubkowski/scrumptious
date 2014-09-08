c35297ddb7e6da5cf4176677ca5b9431c67e2b47
kubkowski <jakub.opara@gmail.com>
2014-09-08 11:15:49 +0200

    Finished Ember course

--- js/models/bookmark.js
+++ js/models/bookmark.js
@@ -1,4 +1,8 @@
 App.Bookmark = DS.Model.extend({
 	title: DS.attr("string"),
-	url: DS.attr("string")
+	url: DS.attr("string"),
+	favorite: DS.attr("boolean"),
+	changedFavorite: function (){
+		this.get("transaction").commit();
+	}.observes("favorite")
 });
